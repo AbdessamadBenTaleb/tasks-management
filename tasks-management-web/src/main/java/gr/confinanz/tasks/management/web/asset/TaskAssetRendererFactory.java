804ffe60fc6a67da41c8707492ffa7f09dcfeff1
AbdessamadBenTaleb <ben-abdo@hotmail.fr>
2019-04-24 20:57:55 +0200

    source format

--- tasks-management-web/src/main/java/gr/confinanz/tasks/management/web/asset/TaskAssetRendererFactory.java
+++ tasks-management-web/src/main/java/gr/confinanz/tasks/management/web/asset/TaskAssetRendererFactory.java
@@ -37,98 +37,98 @@ import gr.confinanz.tasks.management.service.permission.TaskResourcePermissionCh
 		property = {"javax.portlet.name=" + TasksManagementPortletKeys.TASKS_MANAGEMENT},
 		service = AssetRendererFactory.class
 	)
-	public class TaskAssetRendererFactory extends BaseAssetRendererFactory<Task> {
+public class TaskAssetRendererFactory extends BaseAssetRendererFactory<Task> {
 
 		public static final String CLASS_NAME = Task.class.getName();
 
 		public static final String TYPE = "task";
 
-		public TaskAssetRendererFactory() {
-			setClassName(CLASS_NAME);
-			setLinkable(true);
-			setPortletId(TasksManagementPortletKeys.TASKS_MANAGEMENT);
-			setSearchable(true);
-		}
-
-		@Override
-		public AssetRenderer<Task> getAssetRenderer(long classPK, int type)
-			throws PortalException {
-
-			Task task = _taskService.getTask(classPK);
-
-			TaskAssetRenderer taskAssetRenderer = new TaskAssetRenderer(task);
-
-			taskAssetRenderer.setAssetRendererType(type);
-
-			return taskAssetRenderer;
-		}
-
-		@Override
-		public String getType() {
-			return TYPE;
-		}
-
-		@Override
-		public PortletURL getURLAdd(
-			LiferayPortletRequest liferayPortletRequest,
-			LiferayPortletResponse liferayPortletResponse, long classTypeId) {
-			
-			
-			try {
-				long groupId = PortalUtil.getScopeGroupId(liferayPortletRequest);
-				
-				long plid = PortalUtil.getPlidFromPortletId(groupId,
-					TasksManagementPortletKeys.TASKS_MANAGEMENT);
-
-				PortletURL portletURL = PortletURLFactoryUtil.create(
-					liferayPortletRequest, TasksManagementPortletKeys.TASKS_MANAGEMENT,
-					plid, PortletRequest.RENDER_PHASE);
-
-				portletURL.setParameter(
-					"mvcRenderCommandName", "/tasks-management/task/edit");
-
-				return portletURL;
-			}
-			catch (Exception e) {
-				_log.error(e, e);
-			}
-
-			return null;
-		}
-
-		@Override
-		public boolean hasAddPermission(
-				PermissionChecker permissionChecker, long groupId, long classTypeId)
-			throws Exception {
-
-			return TaskResourcePermissionChecker.contains(
-				permissionChecker, groupId, ActionKeys.ADD_TASK);
-		}
-
-		@Override
-		public boolean hasPermission(
-				PermissionChecker permissionChecker, long classPK, String actionId)
-			throws Exception {
-
-			Task task = _taskLocalService.getTask(classPK);
-
-			return TaskPermissionChecker.contains(
-				permissionChecker, task, actionId);
-		}
-
-		private static Log _log = LogFactoryUtil.getLog(
-			TaskAssetRendererFactory.class);
-
-		@Reference(
-			policyOption = ReferencePolicyOption.GREEDY,
-			target = "(osgi.web.symbolicname=gr.confinanz.tasks.management.web)"
-		)
-		private ServletContext _servletContext;
-
-		@Reference(policyOption = ReferencePolicyOption.GREEDY)
-		private TaskLocalService _taskLocalService;
-
-		@Reference(policyOption = ReferencePolicyOption.GREEDY)
-		private TaskService _taskService;
-
+	public TaskAssetRendererFactory() {
+		setClassName(CLASS_NAME);
+		setLinkable(true);
+		setPortletId(TasksManagementPortletKeys.TASKS_MANAGEMENT);
+		setSearchable(true);
 	}
+
+	@Override
+	public AssetRenderer<Task> getAssetRenderer(long classPK, int type)
+		throws PortalException {
+
+		Task task = _taskService.getTask(classPK);
+
+		TaskAssetRenderer taskAssetRenderer = new TaskAssetRenderer(task);
+
+		taskAssetRenderer.setAssetRendererType(type);
+
+		return taskAssetRenderer;
+	}
+
+	@Override
+	public String getType() {
+		return TYPE;
+	}
+
+	@Override
+	public PortletURL getURLAdd(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse, long classTypeId) {
+		
+		
+		try {
+			long groupId = PortalUtil.getScopeGroupId(liferayPortletRequest);
+			
+			long plid = PortalUtil.getPlidFromPortletId(groupId,
+				TasksManagementPortletKeys.TASKS_MANAGEMENT);
+
+			PortletURL portletURL = PortletURLFactoryUtil.create(
+				liferayPortletRequest, TasksManagementPortletKeys.TASKS_MANAGEMENT,
+				plid, PortletRequest.RENDER_PHASE);
+
+			portletURL.setParameter(
+				"mvcRenderCommandName", "/tasks-management/task/edit");
+
+			return portletURL;
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		return null;
+	}
+
+	@Override
+	public boolean hasAddPermission(
+			PermissionChecker permissionChecker, long groupId, long classTypeId)
+		throws Exception {
+
+		return TaskResourcePermissionChecker.contains(
+			permissionChecker, groupId, ActionKeys.ADD_TASK);
+	}
+
+	@Override
+	public boolean hasPermission(
+			PermissionChecker permissionChecker, long classPK, String actionId)
+		throws Exception {
+
+		Task task = _taskLocalService.getTask(classPK);
+
+		return TaskPermissionChecker.contains(
+			permissionChecker, task, actionId);
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		TaskAssetRendererFactory.class);
+
+	@Reference(
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(osgi.web.symbolicname=gr.confinanz.tasks.management.web)"
+	)
+	private ServletContext _servletContext;
+
+	@Reference(policyOption = ReferencePolicyOption.GREEDY)
+	private TaskLocalService _taskLocalService;
+
+	@Reference(policyOption = ReferencePolicyOption.GREEDY)
+	private TaskService _taskService;
+
+}
